f25c12b1a07d91c466526dbe5d8110461624cb13
RobertCarleton <123363877+RobertCarleton@users.noreply.github.com>
2024-05-07 21:30:12 +0100

    Base Done
    3 Pop Ups Complete, with deactivated buttons so only one pop up may exist at once

---
+++ CocktailApp/CocktailApp/HomePage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CocktailApp
+{
+    public partial class HomePage : Page, INotifyPropertyChanged
+    {
+        private bool isPopupOpen;
+        public bool IsPopupOpen
+        {
+            get { return isPopupOpen; }
+            set
+            {
+                if (isPopupOpen != value)
+                {
+                    isPopupOpen = value;
+                    OnPropertyChanged(nameof(IsPopupOpen));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public HomePage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        /*private void ViewRecipes_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to the RecipesPage
+            //NavigationService?.Navigate(new RecipesPage());
+
+        }*/
+        private void ViewPassion_Click(object sender, RoutedEventArgs e)
+        {
+            Passion.IsOpen = true; // Open the popup
+            IsPopupOpen = true;
+        }
+        private void ClosePassion_Click(object sender, RoutedEventArgs e)
+        {
+            Passion.IsOpen = false; // Close the popup
+            IsPopupOpen = false;
+        }
+        private void ViewCosmo_Click(object sender, RoutedEventArgs e)
+        {
+            Cosmo.IsOpen = true; // Open the popup
+            IsPopupOpen = true;
+        }
+        private void CloseCosmo_Click(object sender, RoutedEventArgs e)
+        {
+            Cosmo.IsOpen = false; // Close the popup
+            IsPopupOpen = false;
+        }
+        private void ViewFash_Click(object sender, RoutedEventArgs e)
+        {
+            Fash.IsOpen = true; // Open the popup
+            IsPopupOpen = true;
+        }
+        private void CloseFash_Click(object sender, RoutedEventArgs e)
+        {
+            Fash.IsOpen = false; // Close the popup
+            IsPopupOpen = false;
+        }
+
+    }
+}
